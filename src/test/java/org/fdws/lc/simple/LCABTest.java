6983b4f6c686be1819b3a1e60f5c06ead67aca10
FDws <885374150@qq.com>
2021-03-10 22:47:53 +0800

    lca

---
+++ src/test/java/org/fdws/lc/simple/LCABTest.java
@@ -0,0 +1,38 @@
+package org.fdws.lc.simple;
+
+/**
+ * 二叉搜索树的最近公共祖先
+ *
+ * @author FDws
+ * @version Created on 2021/3/10 22:16
+ */
+
+public class LCABTest {
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == p || root == q || root == null) {
+      return root;
+    }
+    final var left = lowestCommonAncestor(root.left, p, q);
+    final var right = lowestCommonAncestor(root.right, p, q);
+    if (left != null && right != null) {
+      return root;
+    }
+
+    if (left == null && right != null) {
+      return right;
+    }
+    return left;
+  }
+
+
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+}
